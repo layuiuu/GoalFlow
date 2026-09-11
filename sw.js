3b38e28ffa4aeeec4cde2583b44d51aa969e94aa
goalflow <goalflow@users.noreply.github.com>
2026-09-12 03:07:27 +0800

    chore: SW v1.7.0

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * ========================================================== */
 'use strict';
 
-var VERSION = 'v1.6.0';
+var VERSION = 'v1.7.0';
 var CACHE = 'goalflow-' + VERSION;
 
 var ASSETS = [
