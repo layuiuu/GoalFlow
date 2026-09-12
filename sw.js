a17010e1a176b1fa92bef83eaefe5b117cd9c776
goalflow <goalflow@users.noreply.github.com>
2026-09-12 22:02:16 +0800

    chore: SW v1.9.0

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * ========================================================== */
 'use strict';
 
-var VERSION = 'v1.8.0';
+var VERSION = 'v1.9.0';
 var CACHE = 'goalflow-' + VERSION;
 
 var ASSETS = [
